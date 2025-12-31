5c082687e692c637c42fd96311f5561a8680cb4e
Marcos Rodríguez Vega <mrodrive@ull.edu.es>
2025-12-31 16:51:05 +0000

    estudio del EDA

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+pg_intro = st.Page("intro.py", title="Introducción")
+
+# Paginas EDA
+pg_eda_intro = st.Page("seccion_eda/intro_eda.py", title="Primeros Pasos")
+pg_eda_basica = st.Page("seccion_eda/estadisticos_basicos.py", title="Informacion básica")
+
+
+navigation_env = st.navigation(
+    {
+        "": [pg_intro],
+        "EDA": [pg_eda_intro, pg_eda_basica]
+
+    }
+)
+
+navigation_env.run()
